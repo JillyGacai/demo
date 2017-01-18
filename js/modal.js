68f5bd25a2932958423e46ccc7ca6b0c7dfa4d3f
嘎菜 <jyf@umscloud.com>
2017-01-18 16:56:47 +0800

    优化弹框

--- js/modal.js
+++ js/modal.js
@@ -71,10 +71,14 @@ $(function() {
         $('.modal').trigger('modal.hide');
     }).on('click', '.modal-footer .btn', function() {
         $('.modal').trigger('modal.hide');
+    }).on('click', '.hint-close', function() {
+        $(this).closest('.hint').hide();
     });
 
     // backdrop
     $('.modal-backdrop').on('click', function() {});
+
+    // 其他事件 ...
 
 });
 
@@ -85,6 +89,10 @@ $(function() {
  * 尺寸: default(480px) small(360px) large(560px) xlarge(720px)
  * 参数:
  *   option:
+ *     1-文本: name 必须，类名
+ *     实例:
+ *     Modal(name);
+ *     2-对象: 如下
  *     name: 必须 类名, 避免冲突
  *     size: 非必须 尺寸: default(480px) small(360px) large(560px) xlarge(720px)
  *     title: 非必须 标题: 提示(默认)
@@ -93,23 +101,25 @@ $(function() {
  *     sureCb: 非必须 确定按钮回调函数, 默认关闭弹框
  *     cancelName: 非必须 取消按钮名称: 取消(默认)
  *     cancelCb: 非必须 取消按钮回调函数, 默认关闭弹框
- * 备注: 其中 name size title content sureName cancelName 可以直接写在html中
- * 实例:
- *   Modal({
- *      name: 'name',
- *      size: '',
- *      title: '提示',
- *      content: '点击确定按钮弹出Alert',
- *      sureName: '确定',
- *      sureCb: function() {
- *          Alert('this is Alert ...');
- *          return false;
- *      },
- *      cancelName: '取消',
- *      cancelCb: function() {
- *          return true;
- *      }
- *   });
+ *     备注: 其中 name size title content sureName cancelName 可以直接写在html中
+ *     实例:
+ *     Modal({
+ *        name: 'name',
+ *        size: '',
+ *        title: '提示',
+ *        content: '点击确定按钮弹出Alert',
+ *        sureName: '确定',
+ *        sureCb: function() {
+ *           Alert('this is Alert ...');
+ *           return false;
+ *        },
+ *        cancelName: '取消',
+ *        cancelCb: function() {
+ *            return true;
+ *        }
+ *     });
+ *  其他: 单独关闭可执行
+ *      $('.modal.' + name).trigger('modal.hide');
  */
 var Modal = function(option) {
     var modal = '.modal';
@@ -171,6 +181,8 @@ var Modal = function(option) {
  *          return false;
  *      }
  *   });
+ *  其他: 单独关闭可执行
+ *      $('.modal[data-type="confirm"]').trigger('modal.hide');
  */
 var Confirm = function(option) {
     var $confirm = $('.modal[data-type="confirm"]');
@@ -195,16 +207,36 @@ var Confirm = function(option) {
  * 数量: 一个
  * 尺寸: small(360px)
  * 参数:
- *   content: 必须 文本或html
- *   callback: 非必须 回调函数
+ *   option:
+ *      1-文本:直接作为内容;
+ *      实例:
+ *          Alert('这里是内容');
+ *      2-对象:
+ *          content: 必须 文本或html
+ *          callBack: 非必须 回调函数
+ *      实例:
+ *          Alert({
+ *              content: '这里是内容',
+ *              callBack: function() {
+ *                  return false;
+ *              }
+ *          });
+ *  其他: 单独关闭可执行
+ *      $('.modal[data-type="alert"]').trigger('modal.hide');
  */
-var Alert = function(content, callback) {
+var Alert = function(option) {
     var $alert = $('.modal[data-type="alert"]');
-    $alert.find('.modal-body').html(content || '内容');
+    // 内容
+    if (typeof option == 'string') {
+        $alert.find('.modal-body').html(option || '内容');
+    }
+    if (typeof option.content != 'undefined') {
+        $alert.find('.modal-body').html(option.content || '内容');
+    }
     $alert.trigger('modal.show');
     $alert.on('modal.hide', function() {
-        if (typeof callback == 'function') {
-            callback();
+        if (typeof option.callBack == 'function') {
+            option.callBack();
         }
     });
     // 2秒自动关闭
